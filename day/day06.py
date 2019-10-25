cd1ca8a45dd4fd2c0deb451a9c70e1983430dd95
Sun yuwen <630049701@qq.com>
2019-10-25 14:45:23 +0800

    all

---
+++ day/day06.py
@@ -0,0 +1,17 @@
+m = int(input('m = '))
+n = int(input('n = '))
+k = (raw_input('k = '))
+fm = 1
+print(" k = "+k)
+# for num in range(1, m + 1):
+#     fm *= num
+#
+# fn = 1
+# for num in range(1, n + 1):
+#     fn *= num
+#
+# fmn = 1
+# for num in range(1, m - n + 1):
+#     fmn *= num
+#
+# print(fm // fn // fmn)
